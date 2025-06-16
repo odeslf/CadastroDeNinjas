24fd42e558ed94449e38002176fbad86af7948e9
Felipe <lipe13silva@hotmail.com>
2025-06-16 15:33:24 -0300

    feat: Criado MissoesRepository

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,16 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MissoesController {
+
+    @GetMapping("/missoes")
+    public String missoes(){
+        return "Aqui ficam as missoes";
+    }
+}
